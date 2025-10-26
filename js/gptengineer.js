48f0c2998ad3d1e232c8cc79343724e85aec4921
username <163383882+itxusername@users.noreply.github.com>
2025-10-26 16:07:26 -0700

    Create gptengineer.js

---
+++ js/gptengineer.js
@@ -0,0 +1,30 @@
+{
+  "case": 13,
+  "ext": "js",
+  "support": {
+    "id": 45,
+    "cat": "plain",
+    "dcat": "source code"
+  },
+  "types": [
+    {
+      "ID": 45,
+      "Ext": "js",
+      "Type": "JavaScript File",
+      "Category": "Web",
+      "Common": "5",
+      "Summary": "A JS file is a plain text file that contains <a href=\"https://techterms.com/definition/javascript\" target=\"techterms\">JavaScript</a> code. It is used to execute JavaScript instructions in a webpage. JS files may include functions that open and close windows, validate form fields, enable rollover images, or create dropdown menus.",
+      "Icon": "https://fileinfo.com/img/icons/files/128/js-45.png",
+      "LargeIcon": "https://fileinfo.com/img/icons/files/256/js-45.png"
+    },
+    {
+      "ID": 1519,
+      "Ext": "js",
+      "Type": "JScript Executable Script",
+      "Category": "Executable",
+      "Common": "2",
+      "Summary": "A JS file is a (script|script) written in JScript, a Microsoft implementation of (JavaScript|javascript) designed to run on Windows-based systems.  JS files contain executable code that automates tasks primarily in web development or Windows applications.  For example, users may (embed|embed) JS scripts in .HTML webpages for form validation or automate tasks in Windows to manage files."
+    }
+  ],
+  "notice": null
+}
